e64b5e63edb7fab3c33941d3decf9dc4e27c8fc2
ClimaxK <s930415@yahoo.com.tw>
2016-05-12 15:52:33 +0800

    增加SQL備份檔
    Signed-off-by: ClimaxK <s930415@yahoo.com.tw>

---
+++ Go2DrinkLibrary/Go2Drink 20160512 1551.sql
@@ -0,0 +1,61 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.7.12-log
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema go_2_drink
+--
+
+CREATE DATABASE IF NOT EXISTS go_2_drink;
+USE go_2_drink;
+
+--
+-- Definition of table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer` (
+  `email` varchar(45) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `address` varchar(120) NOT NULL,
+  `phone` varchar(45) NOT NULL,
+  `status` int(10) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `Index_u_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customer`
+--
+
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` (`email`,`name`,`gender`,`password`,`birthday`,`address`,`phone`,`status`) VALUES 
+ ('qwer@yahoo.com.tw','林筱筱','F','123456','2016-05-12','台北市萬華區','0229142525',1),
+ ('thewolf@yahoo.com.tw','王瑞玄','M','123456','2016-05-12','台北市復興北路99號14F','0225149191',1);
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Go2DrinkLibrary/go_2_drink .sql
+++ Go2DrinkLibrary/go_2_drink .sql
@@ -1,18 +1,19 @@
-CREATE DATABASE `go_2_drink` /*!40100 DEFAULT CHARACTER SET utf8 */;
+DROP DATABASE IF EXISTS go_2_drink
+CREATE DATABASE go_2_drink /*!40100 DEFAULT CHARACTER SET utf8 */;
 COMMIT;
 USE go_2_drink;
 
-DROP TABLE IF EXISTS `go_2_drink`.`customer`;
-CREATE TABLE  `go_2_drink`.`customer` (
-  `email` varchar(45) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `gender` char(1) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `birthday` date DEFAULT NULL,
-  `address` varchar(120) NOT NULL,
-  `phone` varchar(45) NOT NULL,
-  `status` int(10) unsigned NOT NULL DEFAULT '1',
-  PRIMARY KEY (`email`),
-  UNIQUE KEY `Index_u_email` (`email`)
+DROP TABLE IF EXISTS go_2_drink.customer;
+CREATE TABLE  go_2_drink.customer (
+  email varchar(45) NOT NULL,
+  name varchar(20) NOT NULL,
+  gender char(1) NOT NULL,
+  password varchar(20) NOT NULL,
+  birthday date DEFAULT NULL,
+  address varchar(120) NOT NULL,
+  phone varchar(45) NOT NULL,
+  status int(10) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (email),
+  UNIQUE KEY Index_u_email (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 COMMIT;
